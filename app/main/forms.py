0cce493c1e4d5d1a19c18593e3a33664e34eb870
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-28 12:40:49 +0300

    Add a content form function

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,6 +8,11 @@ class PostForm(FlaskForm): #create a class that inherits from FlaskForm class
     category = SelectField('Choose Blog Category', choices =[('General','General'),('Cars','Cars'),('Technology','Technology')],validators=[Required()])
     post = TextAreaField('Type Blog Post Below:', validators=[Required()])
     submit = SubmitField('Submit')
+    
 class CommentsForm(FlaskForm):
     comments = TextAreaField('Comment on the Post', validators=[Required()])
     submit = SubmitField('Submit')
+
+class ContentForm(FlaskForm):
+    content = TextAreaField('YOUR POST')
+    submit = SubmitField('SUBMIT')
