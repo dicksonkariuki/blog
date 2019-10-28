af4e48ab9314f0218c6763ef753a557f2c1ce7b6
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-28 18:42:44 +0300

    Add blog category

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,10 +5,10 @@ from wtforms import ValidationError
 from wtforms.validators import Required, Email, EqualTo
 
 class PostForm(FlaskForm): #create a class that inherits from FlaskForm class
-    category = SelectField('Choose Blog Category', choices =[('General','General'),('Cars','Cars'),('Technology','Technology')],validators=[Required()])
+    category = SelectField('Choose Blog Category', choices =[('General','General'),('Cars','Cars'),('Technology','Technology')('Lifestyle','Lifestyle')],validators=[Required()])
     post = TextAreaField('Type Blog Post Below:', validators=[Required()])
     submit = SubmitField('Submit')
-    
+
 class CommentsForm(FlaskForm):
     comments = TextAreaField('Comment on the Post', validators=[Required()])
     submit = SubmitField('Submit')
