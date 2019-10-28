b594d38e4ac455d8dcc9f2c77373787c95b3e914
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-28 11:55:24 +0300

    Add flask security module

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
 from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
 class User(UserMixin db.model):
     __tablename__ = 'users'
     """
