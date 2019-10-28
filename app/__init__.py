8836c9c7411c453b209208f6a2e4e070d5d64a86
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-28 12:35:41 +0300

    defining the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,42 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap 
-from .Config import DevConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view ='auth.login'
-# initializing the application
-def create_app(config_name)
+# Initiating login manager
+login_manager=LoginManager()
 
 
+# Initialaizing Flask extensions
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
-from app import views
-#setting up the configuration
-# initializing flask extensions
-bootstrap = Bootstrap(app)
-bootstrap.init_app(app)
-login_manager.init_app(app)
+def create_app(config_name):
+    app = Flask(__name__)
+
+# Creating app configurations
+    app.config.from_object(config_options[config_name])
+    # config_options[config_name].init_app(app)
+
+
+    # Initializing bootstrap
+    bootstrap.init_app(app)
+
+    # Initializing sqlalchemy database
+    db.init_app(app)
+    login_manager.init_app(app)
+    # Registering blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Registering the authentication blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # Initializing the login manager
+    # login_manager.init_app(app)
+
+
+    return app
