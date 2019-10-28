394ee35deafed71b93d0093f07aa2581a9a398b6
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-28 12:38:24 +0300

    Add a postform function

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,3 +4,7 @@ from ..models import User
 from wtforms import ValidationError
 from wtforms.validators import Required, Email, EqualTo
 
+class PostForm(FlaskForm): #create a class that inherits from FlaskForm class
+    category = SelectField('Choose Blog Category', choices =[('General','General'),('Cars','Cars'),('Technology','Technology')],validators=[Required()])
+    post = TextAreaField('Type Blog Post Below:', validators=[Required()])
+    submit = SubmitField('Submit')
